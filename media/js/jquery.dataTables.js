2b126277fcc594174add7407fee2189c4aac980b
Allan Jardine <allan.jardine@sprymedia.co.uk>
2012-02-01 14:03:06 +0000

    DataTables 1.9.0! :-)

--- media/js/jquery.dataTables.js
+++ media/js/jquery.dataTables.js
@@ -1,7 +1,7 @@
 /**
  * @summary     DataTables
  * @description Paginate, search and sort HTML tables
- * @version     1.9.0.dev.4
+ * @version     1.9.0
  * @file        jquery.dataTables.js
  * @author      Allan Jardine (www.sprymedia.co.uk)
  * @contact     www.sprymedia.co.uk/contact
@@ -6542,7 +6542,7 @@
 	 *  @type string
 	 *  @default Version number
 	 */
-	DataTable.version = "1.9.0.dev.4";
+	DataTable.version = "1.9.0";
 
 	/**
 	 * Private data store, containing all of the settings objects that are created for the
