DataTables/DataTables

commit 876a75f6ad046ffe699e8c0e4702fc132b61dda3

Fix: With scrolling enabled, adding the scrollbar width to the header / footer inner container elements could cause the table to resize incorrectly on the following draw (and this error was cumulative). Fix is not to adjust the inner element for the scrollbar width - makes no difference to the table draw. It is possible that if you've styled this element you might need to take this change into account, but by default DataTables will style the parent (scrollHead) so no change is required (regardless of jQuery UI theming enablement) - 6776. Fix: The calculation to detect if the scroll bar would be shown in IE6/7 was incorrect - it was calculating the height of the entire table, rather than just the body of the table (i.e. body + header + footer) which caused the "correct" for the scrollbar to be incorrectly applied to small tables.

media/js/jquery.dataTables.js
@@ -3127,8 +3127,8 @@
 				 * the scrollbar - which is shouldn't. When there is a scrollbar we need to take this
 				 * into account.
 				 */
-				if ( ie67 && (nScrollBody.scrollHeight > 
-					nScrollBody.offsetHeight || $(nScrollBody).css('overflow-y') == "scroll")  )
+				if ( ie67 && ($('tbody', nScrollBody).height() > nScrollBody.offsetHeight || 
+					$(nScrollBody).css('overflow-y') == "scroll")  )
 				{
 					o.nTable.style.width = _fnStringToCss( $(o.nTable).outerWidth()-o.oScroll.iBarWidth );
 				}
@@ -3279,7 +3279,6 @@
 			/*
 			 * 4. Clean up
 			 */
-			
 			if ( o.oScroll.sY === "" )
 			{
 				/* IE7< puts a vertical scrollbar in place (when it shouldn't be) due to subtracting
@@ -3307,11 +3306,11 @@
 			/* Finally set the width's of the header and footer tables */
 			var iOuterWidth = $(o.nTable).outerWidth();
 			nScrollHeadTable.style.width = _fnStringToCss( iOuterWidth );
-			nScrollHeadInner.style.width = _fnStringToCss( iOuterWidth+o.oScroll.iBarWidth );
+			nScrollHeadInner.style.width = _fnStringToCss( iOuterWidth );
 			
 			if ( o.nTFoot !== null )
 			{
-				nScrollFootInner.style.width = _fnStringToCss( o.nTable.offsetWidth+o.oScroll.iBarWidth );
+				nScrollFootInner.style.width = _fnStringToCss( o.nTable.offsetWidth );
 				nScrollFootTable.style.width = _fnStringToCss( o.nTable.offsetWidth );
 			}
 			

media/src/core/core.scrolling.js
@@ -256,8 +256,8 @@ function _fnScrollDraw ( o )
 		 * the scrollbar - which is shouldn't. When there is a scrollbar we need to take this
 		 * into account.
 		 */
-		if ( ie67 && (nScrollBody.scrollHeight > 
-			nScrollBody.offsetHeight || $(nScrollBody).css('overflow-y') == "scroll")  )
+		if ( ie67 && ($('tbody', nScrollBody).height() > nScrollBody.offsetHeight || 
+			$(nScrollBody).css('overflow-y') == "scroll")  )
 		{
 			o.nTable.style.width = _fnStringToCss( $(o.nTable).outerWidth()-o.oScroll.iBarWidth );
 		}
@@ -408,7 +408,6 @@ function _fnScrollDraw ( o )
 	/*
 	 * 4. Clean up
 	 */
-	
 	if ( o.oScroll.sY === "" )
 	{
 		/* IE7< puts a vertical scrollbar in place (when it shouldn't be) due to subtracting
@@ -436,11 +435,11 @@ function _fnScrollDraw ( o )
 	/* Finally set the width's of the header and footer tables */
 	var iOuterWidth = $(o.nTable).outerWidth();
 	nScrollHeadTable.style.width = _fnStringToCss( iOuterWidth );
-	nScrollHeadInner.style.width = _fnStringToCss( iOuterWidth+o.oScroll.iBarWidth );
+	nScrollHeadInner.style.width = _fnStringToCss( iOuterWidth );
 	
 	if ( o.nTFoot !== null )
 	{
-		nScrollFootInner.style.width = _fnStringToCss( o.nTable.offsetWidth+o.oScroll.iBarWidth );
+		nScrollFootInner.style.width = _fnStringToCss( o.nTable.offsetWidth );
 		nScrollFootTable.style.width = _fnStringToCss( o.nTable.offsetWidth );
 	}
 	

commit ab9dfd50529de0daa8bea9d4e8f1035f2b5e6e52

Fix: x-scrolling to the end of a table would cause the header and the columns to go out of alignment due to the header not being able to scroll as far as was needed (this was caused by the changes in 876a75f) - 8332. Fix: Table could conitnually expand when x-scrolling was enabled. This was partly addressed in 6776, but the fix was incomplete as it would still occur on Safari Mac (possibly other browsers as well). This fix is very closely related to 8332 (hence commiting together as they are interdependent). Now use padding right on the header/footer wrapper to provide the overflow scroll ability, but only add it when a scrollbar is present - otherwise the width gets added on and we get the forever expanding table. Dev: Unit tests - New tests for scrolling to ensure 6776 and 8332 don't occur again

media/js/jquery.dataTables.js
@@ -3128,7 +3128,7 @@
 				if ( ie67 && ($('tbody', nScrollBody).height() > nScrollBody.offsetHeight || 
 					$(nScrollBody).css('overflow-y') == "scroll")  )
 				{
-					o.nTable.style.width = _fnStringToCss( $(o.nTable).outerWidth()-o.oScroll.iBarWidth );
+					o.nTable.style.width = _fnStringToCss( $(o.nTable).outerWidth() - o.oScroll.iBarWidth);
 				}
 			}
 			else
@@ -3305,12 +3305,19 @@
 			var iOuterWidth = $(o.nTable).outerWidth();
 			nScrollHeadTable.style.width = _fnStringToCss( iOuterWidth );
 			nScrollHeadInner.style.width = _fnStringToCss( iOuterWidth );
+		
+			var bScrolling = $(o.nTable).height() > nScrollBody.clientHeight || $(nScrollBody).css('overflow-y') == "scroll";
+			nScrollHeadInner.style.paddingRight = bScrolling ? o.oScroll.iBarWidth+"px" : "0px";
 			
 			if ( o.nTFoot !== null )
 			{
-				nScrollFootInner.style.width = _fnStringToCss( o.nTable.offsetWidth );
-				nScrollFootTable.style.width = _fnStringToCss( o.nTable.offsetWidth );
+				nScrollFootTable.style.width = _fnStringToCss( iOuterWidth );
+				nScrollFootInner.style.width = _fnStringToCss( iOuterWidth );
+				nScrollFootInner.style.paddingRight = bScrolling ? o.oScroll.iBarWidth+"px" : "0px";
 			}
+		
+			/* Adjust the position of the header incase we loose the y-scrollbar */
+			$(nScrollBody).scroll();
 			
 			/* If sorting or filtering has occurred, jump the scrolling back to the top */
 			if ( o.bSorted || o.bFiltered )

media/src/core/core.scrolling.js
@@ -259,7 +259,7 @@ function _fnScrollDraw ( o )
 		if ( ie67 && ($('tbody', nScrollBody).height() > nScrollBody.offsetHeight || 
 			$(nScrollBody).css('overflow-y') == "scroll")  )
 		{
-			o.nTable.style.width = _fnStringToCss( $(o.nTable).outerWidth()-o.oScroll.iBarWidth );
+			o.nTable.style.width = _fnStringToCss( $(o.nTable).outerWidth() - o.oScroll.iBarWidth);
 		}
 	}
 	else
@@ -436,12 +436,21 @@ function _fnScrollDraw ( o )
 	var iOuterWidth = $(o.nTable).outerWidth();
 	nScrollHeadTable.style.width = _fnStringToCss( iOuterWidth );
 	nScrollHeadInner.style.width = _fnStringToCss( iOuterWidth );
+
+	// Figure out if there are scrollbar present - if so then we need a the header and footer to
+	// provide a bit more space to allow "overflow" scrolling (i.e. past the scrollbar)
+	var bScrolling = $(o.nTable).height() > nScrollBody.clientHeight || $(nScrollBody).css('overflow-y') == "scroll";
+	nScrollHeadInner.style.paddingRight = bScrolling ? o.oScroll.iBarWidth+"px" : "0px";
 	
 	if ( o.nTFoot !== null )
 	{
-		nScrollFootInner.style.width = _fnStringToCss( o.nTable.offsetWidth );
-		nScrollFootTable.style.width = _fnStringToCss( o.nTable.offsetWidth );
+		nScrollFootTable.style.width = _fnStringToCss( iOuterWidth );
+		nScrollFootInner.style.width = _fnStringToCss( iOuterWidth );
+		nScrollFootInner.style.paddingRight = bScrolling ? o.oScroll.iBarWidth+"px" : "0px";
 	}
+
+	/* Adjust the position of the header incase we loose the y-scrollbar */
+	$(nScrollBody).scroll();
 	
 	/* If sorting or filtering has occurred, jump the scrolling back to the top */
 	if ( o.bSorted || o.bFiltered )

media/unit_testing/templates/6776.php
@@ -1,0 +1,116 @@
+<?php
+	header( 'Expires: Sat, 26 Jul 1997 05:00:00 GMT' ); 
+	header( 'Last-Modified: ' . gmdate( 'D, d M Y H:i:s' ) . ' GMT' ); 
+	header( 'Cache-Control: no-store, no-cache, must-revalidate' ); 
+	header( 'Cache-Control: post-check=0, pre-check=0', false ); 
+	header( 'Pragma: no-cache' ); 
+?><!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<link rel="shortcut icon" type="image/ico" href="http://www.sprymedia.co.uk/media/images/favicon.ico" />
+		
+		<title>DataTables unit testing</title>
+		<style type="text/css" title="currentStyle">
+			@import "../../css/demo_page.css";
+			@import "../../css/demo_table.css";
+		</style>
+		<script type="text/javascript" language="javascript" src="../../js/jquery.js"></script>
+		<script type="text/javascript" language="javascript" src="../../js/jquery.dataTables.js"></script>
+		<script type="text/javascript" language="javascript" src="../unit_test.js"></script>
+		<?php
+			$aScripts = explode( ":", $_GET['scripts'] );
+			for ( $i=0 ; $i<count($aScripts) ; $i++ )
+			{
+				echo '<script type="text/javascript" language="javascript" src="../'.$aScripts[$i].'?rand='.rand().'"></script>'."\n";
+			}
+		?>
+	</head>
+  <body id="dt_example">
+    <div id="container">
+    <div id="demo">
+      <h1>Live example</h1>
+      <table>
+        <thead/>
+        <tbody>
+          <tr>
+            <td>
+            <div>
+              <table cellpadding="0" cellspacing="0" border="0" class="display" id="example">
+                <thead>
+                  <tr>
+                    <th>Rendering engine</th>
+                    <th width="25%">Browser</th>
+                    <th>Platform(s)</th>
+                    <th>Engine version</th>
+                    <th>CSS grade</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr class="odd gradeX">
+                    <td>Trident</td>
+                    <td>Internet Explorer 4.0</td>
+                    <td>Win 95+</td>
+                    <td class="center"> 4</td>
+                    <td class="center">X</td>
+                  </tr>
+                  <tr class="odd gradeX">
+                    <td>Trident</td>
+                    <td>Internet Explorer 4.0</td>
+                    <td>Win 95+</td>
+                    <td class="center"> 4</td>
+                    <td class="center">X</td>
+                  </tr>
+                  <tr class="odd gradeX">
+                    <td>Trident</td>
+                    <td>Internet Explorer 4.0</td>
+                    <td>Win 95+</td>
+                    <td class="center"> 4</td>
+                    <td class="center">X</td>
+                  </tr>
+                  <tr class="odd gradeX">
+                    <td>Trident</td>
+                    <td>Internet Explorer 4.0</td>
+                    <td>Win 95+</td>
+                    <td class="center"> 4</td>
+                    <td class="center">X</td>
+                  </tr>
+                  <tr class="odd gradeX">
+                    <td>Trident</td>
+                    <td>Internet Explorer 4.0</td>
+                    <td>Win 95+</td>
+                    <td class="center"> 4</td>
+                    <td class="center">X</td>
+                  </tr>
+                  <tr class="odd gradeX">
+                    <td>Trident</td>
+                    <td>Internet Explorer 4.0</td>
+                    <td>Win 95+</td>
+                    <td class="center"> 4</td>
+                    <td class="center">X</td>
+                  </tr>
+                  <tr class="odd gradeX">
+                    <td>Trident</td>
+                    <td>Internet Explorer 4.0</td>
+                    <td>Win 95+</td>
+                    <td class="center"> 4</td>
+                    <td class="center">X</td>
+                  </tr>
+                  <tr class="odd gradeX">
+                    <td>Trident</td>
+                    <td>Internet Explorer 4.0</td>
+                    <td>Win 95+</td>
+                    <td class="center"> 4</td>
+                    <td class="center">X</td>
+                  </tr>
+                </tbody>
+              </table>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    </div>
+  </body>
+</html>

media/unit_testing/tests_onhold/1_dom/6776-scrolling-table-grows.js
@@ -1,0 +1,64 @@
+// DATA_TEMPLATE: 6776
+oTest.fnStart( "Actions on a scrolling table keep width" );
+
+
+$(document).ready( function () {
+	var oTable = $('#example').dataTable( {
+        "bFilter": true,
+        "bSort": true,
+        "sScrollY": "100px",
+        "bPaginate": false
+	} );
+	
+	var iWidth = $('div.dataTables_wrapper').width();
+
+	oTest.fnTest( 
+		"First sort has no effect on width",
+		function () { $('th:eq(1)').click(); },
+		function () { return $('div.dataTables_wrapper').width() == iWidth; }
+	);
+
+	oTest.fnTest( 
+		"Second sort has no effect on width",
+		function () { $('th:eq(1)').click(); },
+		function () { return $('div.dataTables_wrapper').width() == iWidth; }
+	);
+
+	oTest.fnTest( 
+		"Third sort has no effect on width",
+		function () { $('th:eq(2)').click(); },
+		function () { return $('div.dataTables_wrapper').width() == iWidth; }
+	);
+
+	oTest.fnTest( 
+		"Filter has no effect on width",
+		function () { oTable.fnFilter('i'); },
+		function () { return $('div.dataTables_wrapper').width() == iWidth; }
+	);
+
+	oTest.fnTest( 
+		"Filter 2 has no effect on width",
+		function () { oTable.fnFilter('in'); },
+		function () { return $('div.dataTables_wrapper').width() == iWidth; }
+	);
+
+	oTest.fnTest( 
+		"No result filter has header and body at same width",
+		function () { oTable.fnFilter('xxx'); },
+		function () { return $('#example').width() == $('div.dataTables_scrollHeadInner').width(); }
+	);
+
+	oTest.fnTest( 
+		"Filter with no results has no effect on width",
+		function () { oTable.fnFilter('xxx'); },
+		function () { return $('div.dataTables_wrapper').width() == iWidth; }
+	);
+
+	oTest.fnTest( 
+		"Filter with no results has table equal to wrapper width",
+		function () { oTable.fnFilter('xxx'); },
+		function () { return $('div.dataTables_wrapper').width() == $('#example').width(); }
+	);
+	
+	oTest.fnComplete();
+} );

media/unit_testing/tests_onhold/1_dom/sScrollXY.js
@@ -1,0 +1,63 @@
+// DATA_TEMPLATE: dom_data
+oTest.fnStart( "sScrollX / Y" );
+
+
+$(document).ready( function () {
+	// Force some x scrolling
+	$('body').css('white-space', 'nowrap');
+	$('#container').css('width', '400px');
+
+	var oTable = $('#example').dataTable( {
+		"sScrollX": "100%",
+		"sScrollY": "200px",
+		"bPaginate": false
+	} );
+	
+	oTest.fnWaitTest( 
+		"Header follows x-scrolling",
+		function () { $('div.dataTables_scrollBody').scrollLeft(20); },
+		function () { return $('div.dataTables_scrollHead').scrollLeft() == 20; }
+	);
+	
+	oTest.fnWaitTest( 
+		"Footer follows x-scrolling",
+		null,
+		function () { return $('div.dataTables_scrollFoot').scrollLeft() == 20; }
+	);
+	
+	oTest.fnWaitTest( 
+		"y-scrolling has no effect on header",
+		function () { $('div.dataTables_scrollBody').scrollTop(20); },
+		function () { return $('div.dataTables_scrollHead').scrollLeft() == 20; }
+	);
+	
+	oTest.fnWaitTest( 
+		"Filtering results in sets y-scroll back to 0",
+		function () { oTable.fnFilter('1') },
+		function () { return $('div.dataTables_scrollBody').scrollTop() == 0; }
+	);
+	
+	oTest.fnWaitTest( 
+		"Filtering has no effect on x-scroll",
+		null,
+		function () { return $('div.dataTables_scrollBody').scrollLeft() == 20; }
+	);
+	
+	oTest.fnWaitTest( 
+		"Full x-scroll has header track all the way with it",
+		function () {
+			$('div.dataTables_scrollBody').scrollLeft(
+				$('#example').width() - $('div.dataTables_scrollBody')[0].clientWidth
+			);
+		},
+		function () { return $('div.dataTables_scrollBody').scrollLeft() == $('div.dataTables_scrollHead').scrollLeft(); }
+	);
+	
+	oTest.fnTest( 
+		"Footer also tracked all the way",
+		null,
+		function () { return $('div.dataTables_scrollBody').scrollLeft() == $('div.dataTables_scrollFoot').scrollLeft(); }
+	);
+	
+	oTest.fnComplete();
+} );