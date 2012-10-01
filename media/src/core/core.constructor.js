5623664400acfc65aeef70b19c688c36240e72fb
Tim Tucker <tim@timtucker.com>
2012-10-01 07:58:03 -0300

    Update media/src/core/core.constructor.js
    Realized that the !bDestroy wasn't needed, since the original order would still execute properly due to the break.

--- media/src/core/core.constructor.js
+++ media/src/core/core.constructor.js
@@ -23,16 +23,16 @@ for ( i=0, iLen=DataTable.settings.length ; i<iLen ; i++ )
 			return DataTable.settings[i].oInstance;
 		}
 		
-		if ( !oInit.bDestroy )
+		if ( oInit.bDestroy )
 		{
-			_fnLog( DataTable.settings[i], 0, "Cannot reinitialise DataTable.\n\n"+
-				"To retrieve the DataTables object for this table, pass no arguments or see "+
-				"the docs for bRetrieve and bDestroy" );
-			return;
+			DataTable.settings[i].oInstance.fnDestroy();
+			break;
 		}
-		
-		DataTable.settings[i].oInstance.fnDestroy();
-		break;
+
+		_fnLog( DataTable.settings[i], 0, "Cannot reinitialise DataTable.\n\n"+
+			"To retrieve the DataTables object for this table, pass no arguments or see "+
+			"the docs for bRetrieve and bDestroy" );
+		return;
 	}
 	
 	/* If the element we are initialising has the same ID as a table which was previously
