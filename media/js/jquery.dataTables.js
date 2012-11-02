33d3667bbecae845307f7c4f7564d37343ede20a
Allan Jardine <allan.jardine@sprymedia.co.uk>
2012-11-02 08:33:07 +0000

    Update: Much smaller numeric type detection method, based on isNumeric in jQuery. Also a whole lot faster.

--- media/js/jquery.dataTables.js
+++ media/js/jquery.dataTables.js
@@ -11654,53 +11654,13 @@
 		 * Function: -
 		 * Purpose:  Check to see if a string is numeric
 		 * Returns:  string:'numeric' or null
-		 * Inputs:   mixed:sText - string to check
+		 * Inputs:   mixed:data - string to check
 		 */
-		function ( sData )
+		function ( data )
 		{
-			/* Allow zero length strings as a number */
-			if ( typeof sData === 'number' )
-			{
-				return 'numeric';
-			}
-			else if ( typeof sData !== 'string' )
-			{
-				return null;
-			}
-			
-			var sValidFirstChars = "0123456789-";
-			var sValidChars = "0123456789.";
-			var Char;
-			var bDecimal = false;
-			
-			/* Check for a valid first char (no period and allow negatives) */
-			Char = sData.charAt(0); 
-			if (sValidFirstChars.indexOf(Char) == -1) 
-			{
-				return null;
-			}
-			
-			/* Check all the other characters are valid */
-			for ( var i=1 ; i<sData.length ; i++ ) 
-			{
-				Char = sData.charAt(i); 
-				if (sValidChars.indexOf(Char) == -1) 
-				{
-					return null;
-				}
-				
-				/* Only allowed one decimal place... */
-				if ( Char == "." )
-				{
-					if ( bDecimal )
-					{
-						return null;
-					}
-					bDecimal = true;
-				}
-			}
-			
-			return 'numeric';
+			return data==='' || data==='-' || (!isNaN( parseFloat(data) ) && isFinite( data )) ?
+				'numeric' :
+				null;
 		},
 		
 		/*
