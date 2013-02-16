6e012c8ee13ec9f9bcc622741500b326aecc8366
Allan Jardine <allan.jardine@sprymedia.co.uk>
2013-02-16 11:33:50 +0000

    Dev fix: Thousands separator was incorrectly applied
    - This was from my old work on experimenting with removing hugraian
      notation, it slipped through to be committed.

--- media/js/jquery.dataTables.js
+++ media/js/jquery.dataTables.js
@@ -8608,7 +8608,7 @@
 			{
 				if ( i%3 === 0 && i !== 0 )
 				{
-					out = this.language.infoThousands+out;
+					out = this.oLanguage.sInfoThousands+out;
 				}
 				out = a[iLen-i-1]+out;
 			}
--- media/src/model/model.defaults.js
+++ media/src/model/model.defaults.js
@@ -938,7 +938,7 @@ DataTable.defaults = {
 		{
 			if ( i%3 === 0 && i !== 0 )
 			{
-				out = this.language.infoThousands+out;
+				out = this.oLanguage.sInfoThousands+out;
 			}
 			out = a[iLen-i-1]+out;
 		}
