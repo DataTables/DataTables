3827b96b7db9ad9b59613dd811e5d01e716ef1fc
Allan Jardine <allan.jardine@sprymedia.co.uk>
2013-06-19 17:32:24 +0100

    New: jQuery UI integration now has the sorting_* classes applied to it for the header cells (matching the standard case, in addition to the ui-state-default class for styling).
    - This allows the developer to add styling information based on the
      sorting state of the cell.
    
    - This fixed issue #17

--- media/src/ext/ext.classes.js
+++ media/src/ext/ext.classes.js
@@ -54,10 +54,23 @@ $.extend( DataTable.ext.oStdClasses, {
 } );
 
 
+(function() {
+
+// Reused strings for better compression. Closure compiler appears to have a
+// weird edge case where it is trying to expand strings rather than use the
+// variable version. This results in about 200 bytes being added, for very
+// little preference benefit since it this run on script load only.
+var _empty = '';
+_empty = '';
+
+var _stateDefault = _empty + 'ui-state-default';
+var _sortIcon     = _empty + 'css_right ui-icon ui-icon-';
+var _headerFooter = _empty + 'fg-toolbar ui-toolbar ui-widget-header ui-helper-clearfix';
+
 $.extend( DataTable.ext.oJUIClasses, DataTable.ext.oStdClasses, {
 	/* Full numbers paging buttons */
-	"sPageButton": "fg-button ui-button ui-state-default",
-	"sPageButtonActive": "ui-state-disabled",
+	"sPageButton":         "fg-button ui-button "+_stateDefault,
+	"sPageButtonActive":   "ui-state-disabled",
 	"sPageButtonDisabled": "ui-state-disabled",
 
 	/* Features */
@@ -65,28 +78,30 @@ $.extend( DataTable.ext.oJUIClasses, DataTable.ext.oStdClasses, {
 		"ui-buttonset-multi paging_", /* Note that the type is postfixed */
 
 	/* Sorting */
-	"sSortAsc": "ui-state-default",
-	"sSortDesc": "ui-state-default",
-	"sSortable": "ui-state-default",
-	"sSortableAsc": "ui-state-default",
-	"sSortableDesc": "ui-state-default",
-	"sSortableNone": "ui-state-default",
-	"sSortJUIAsc": "css_right ui-icon ui-icon-triangle-1-n",
-	"sSortJUIDesc": "css_right ui-icon ui-icon-triangle-1-s",
-	"sSortJUI": "css_right ui-icon ui-icon-carat-2-n-s",
-	"sSortJUIAscAllowed": "css_right ui-icon ui-icon-carat-1-n",
-	"sSortJUIDescAllowed": "css_right ui-icon ui-icon-carat-1-s",
-	"sSortJUIWrapper": "DataTables_sort_wrapper",
-	"sSortIcon": "DataTables_sort_icon",
+	"sSortAsc":            _stateDefault+" sorting_asc",
+	"sSortDesc":           _stateDefault+" sorting_desc",
+	"sSortable":           _stateDefault+" sorting",
+	"sSortableAsc":        _stateDefault+" sorting_asc_disabled",
+	"sSortableDesc":       _stateDefault+" sorting_desc_disabled",
+	"sSortableNone":       _stateDefault+" sorting_disabled",
+	"sSortJUIAsc":         _sortIcon+"triangle-1-n",
+	"sSortJUIDesc":        _sortIcon+"triangle-1-s",
+	"sSortJUI":            _sortIcon+"carat-2-n-s",
+	"sSortJUIAscAllowed":  _sortIcon+"carat-1-n",
+	"sSortJUIDescAllowed": _sortIcon+"carat-1-s",
+	"sSortJUIWrapper":     "DataTables_sort_wrapper",
+	"sSortIcon":           "DataTables_sort_icon",
 
 	/* Scrolling */
-	"sScrollHead": "dataTables_scrollHead ui-state-default",
-	"sScrollFoot": "dataTables_scrollFoot ui-state-default",
+	"sScrollHead": "dataTables_scrollHead "+_stateDefault,
+	"sScrollFoot": "dataTables_scrollFoot "+_stateDefault,
 
 	/* Misc */
-	"sHeaderTH": "ui-state-default",
-	"sFooterTH": "ui-state-default",
-	"sJUIHeader": "fg-toolbar ui-toolbar ui-widget-header ui-corner-tl ui-corner-tr ui-helper-clearfix",
-	"sJUIFooter": "fg-toolbar ui-toolbar ui-widget-header ui-corner-bl ui-corner-br ui-helper-clearfix"
+	"sHeaderTH":  _stateDefault,
+	"sFooterTH":  _stateDefault,
+	"sJUIHeader": _headerFooter+" ui-corner-tl ui-corner-tr",
+	"sJUIFooter": _headerFooter+" ui-corner-bl ui-corner-br"
 } );
+
+}());
 
