cbeaadcba854b20b3f2230e44d640b0fe7dc0055
Allan Jardine <allan.jardine@sprymedia.co.uk>
2013-08-08 14:51:51 +0100

    Dev fix: Additional to the content-box fix form the other day - use jQuery since browser specific styles can override the W3C option. jQuery deals with it for us.

--- media/js/jquery.dataTables.js
+++ media/js/jquery.dataTables.js
@@ -3009,7 +3009,7 @@
 		nScrollHead.style.border = "0";
 		nScrollHead.style.width = "100%";
 		nScrollFoot.style.border = "0";
-		nScrollHeadInner.style.boxSizing = 'content-box';
+		$(nScrollHeadInner).css('box-sizing', 'content-box');
 		nScrollHeadInner.style.width = oSettings.oScroll.sXInner !== "" ?
 			oSettings.oScroll.sXInner : "100%"; /* will be overwritten */
 	
