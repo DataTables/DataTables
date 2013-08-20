e1f4a8fa9ed41208dc337f0a8d60cbd204443ba0
Allan Jardine <allan.jardine@sprymedia.co.uk>
2013-08-20 15:01:10 +0100

    Use named module for AMD include
    Based on DataTables/DataTables/issues/214 this commit uses the optional id parameter for the AMD define function to give DataTables a 'name' for easy referencing.

--- media/js/jquery.dataTables.js
+++ media/js/jquery.dataTables.js
@@ -31,7 +31,7 @@
 	// Define as an AMD module if possible
 	if ( typeof define === 'function' && define.amd )
 	{
-		define( ['jquery'], factory );
+		define( 'datatables', ['jquery'], factory );
 	}
 	/* Define using browser globals otherwise
 	 * Prevent multiple instantiations if the script is loaded twice
