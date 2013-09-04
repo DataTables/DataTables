19991d4cd1f87b3f337566e991404df0594dba52
Sam Van der Borght <sam@king-foo.be>
2013-09-04 10:59:25 +0200

    Add CSS class to length select element

--- media/js/jquery.dataTables.js
+++ media/js/jquery.dataTables.js
@@ -2751,6 +2751,7 @@
 			'name':          tableId+'_length',
 			'aria-controls': tableId
 		} );
+        select.addClass( settings.oClasses.sLengthMenu);
 	
 		for ( var i=0, ien=lengths.length ; i<ien ; i++ ) {
 			select[0][ i ] = new Option( language[i], lengths[i] );
@@ -13321,6 +13322,7 @@
 		"sInfo": "dataTables_info",
 		"sPaging": "dataTables_paginate paging_", /* Note that the type is postfixed */
 		"sLength": "dataTables_length",
+        "sLengthMenu": "", /* Length menu element */
 		"sProcessing": "dataTables_processing",
 	
 		/* Sorting */
