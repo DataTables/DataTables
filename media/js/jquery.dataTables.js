c916bb05fe264022a8baca00fa4ac1b5b93e8a04
Allan Jardine <allan.jardine@sprymedia.co.uk>
2013-10-15 15:31:35 +0100

    New: search() and column[s]().search() API methods now have a 'get' option
    - By passing in no parameters to the search API methods we can get the
      current search value - for example:
          $('#example').DataTable().search();
    
    - This is to round of the API as the order() method already has a get
      option as to the paging information

--- media/js/jquery.dataTables.js
+++ media/js/jquery.dataTables.js
@@ -8028,6 +8028,16 @@
 	
 	
 	_api_register( 'search()', function ( input, regex, smart, caseInsen ) {
+		var ctx = this.context;
+	
+		if ( input === undefined ) {
+			// get
+			return ctx.length !== 0 ?
+				ctx[0].oPreviousSearch.sSearch :
+				undefined;
+		}
+	
+		// set
 		return this.iterator( 'table', function ( settings ) {
 			if ( ! settings.oFeatures.bFilter ) {
 				return;
@@ -8048,11 +8058,19 @@
 		'column().search()'
 	], function ( input, regex, smart, caseInsen ) {
 		return this.iterator( 'column', function ( settings, column ) {
+			var preSearch = settings.aoPreSearchCols;
+	
+			if ( input === undefined ) {
+				// get
+				return preSearch[ column ].sSearch;
+			}
+	
+			// set
 			if ( ! settings.oFeatures.bFilter ) {
 				return;
 			}
 	
-			$.extend( settings.aoPreSearchCols[ column ], {
+			$.extend( preSearch[ column ], {
 				"sSearch": input+"",
 				"bRegex":  regex === null ? false : regex,
 				"bSmart":  smart === null ? true  : smart,
