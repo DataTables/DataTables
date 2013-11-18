635a027057e891c96d87cfcb00cecf48e3ae6f41
Allan Jardine <allan.jardine@sprymedia.co.uk>
2013-11-18 11:46:15 +0000

    Dev: Remove the static `construct` method that had been added as part of 1.10
    - I added a static DataTable.on() method previously in the 1.10
      development for a single event called `construct` which would tell us
      when a table was constructed. But I've realise that since the events
      that DataTables' triggers bubble up through the document, that effect
      can already be achieved:
    
    - Use:
      - $(document).on( 'init.dt', function ( e, settings ) { } );
    
    - Rather than:
      - $.fn.dataTable.on( 'construct', function ( settings ) { } );
    
    - No backwards compatiblity issues as the DataTable.on code was never
      shipped, its dev code only

--- media/js/jquery.dataTables.js
+++ media/js/jquery.dataTables.js
@@ -3003,10 +3003,6 @@
 		}
 	
 		_fnCallbackFire( settings, 'aoInitComplete', 'init', [settings, json] );
-	
-		$.each( DataTable._initQueue, function ( i, fn ) {
-			fn( settings );
-		} );
 	}
 	
 	
@@ -8280,35 +8276,6 @@
 		} );
 	
 		return out;
-	};
-	
-	
-	// Private property used for sorting the construct event listeners
-	DataTable._initQueue = [];
-	
-	/**
-	 * Listen for a static event from DataTables. Currently only one event is
-	 * available: `construct` which is fired whenever a new table is created. This
-	 * is for plug-ins to be able to automatically initialise themselves when a
-	 * table is created.
-	 *
-	 *  @param {string} ev Event to listen for. Currently only `construct` is
-	 *    available.
-	 *  @param {function} fn Function to be executed on event occurrence. A single
-	 *    parameter is passed in, the settings object for the new table.
-	 *  @static
-	 *  @dtopt API-Static
-	 *
-	 *  @example
-	 *    $.fn.dataTable.on( 'construct', function ( settings ) {
-	 *      alert( 'A new table was created' );
-	 *    } );
-	 */
-	DataTable.on = function ( ev, fn )
-	{
-		if ( ev === 'construct' ) {
-			DataTable._initQueue.push( fn );
-		}
 	};
 	
 	
