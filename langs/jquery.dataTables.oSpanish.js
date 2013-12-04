fb35a4d53a1acaeedf69d9e96dc02769dbf1fad1
Bruno Alassia <vruno.alassia@gmail.com>
2013-12-04 10:18:05 -0300

    add spanish language

---
+++ langs/jquery.dataTables.oSpanish.js
@@ -0,0 +1,25 @@
+// TODO - improve the language selection, use extend
+var oSpanish = {
+	"oAria": {
+		"sSortAscending": ": activate to sort column ascending",
+		"sSortDescending": ": activate to sort column descending"
+	},
+	"oPaginate": {
+		"sFirst": "Primero",
+		"sLast": "Ultimo",
+		"sNext": "Siguiente",
+		"sPrevious": "Anterior"
+	},
+	"sEmptyTable": "Sin datos disponibles para la tabla",
+	"sInfo": "Mostrando _START_ a _END_ de _TOTAL_ resultados",
+	"sInfoEmpty": "Mostrando 0 a 0 de 0 resultados",
+	"sInfoFiltered": "(filtrando _MAX_ de resultados)",
+	"sInfoPostFix": "",
+	"sInfoThousands": ",",
+	"sLengthMenu": "Mostrar _MENU_ resultados",
+	"sLoadingRecords": "Cargando...",
+	"sProcessing": "Procesando...",
+	"sSearch": "Buscar:",
+	"sUrl": "",
+	"sZeroRecords": "No se han encontrado resultados"
+}
