75d9800522889f08210fcfea738dffa9dc4e3e65
Allan Jardine <allan.jardine@sprymedia.co.uk>
2013-12-19 14:04:14 +0000

    Site: Correct documentation for `ajax` and small updates for site

--- examples/resources/demo.js
+++ examples/resources/demo.js
@@ -6,8 +6,8 @@ $(document).ready( function () {
 	// Work around for WebKit bug 55740
 	var info = $('div.info');
 
-	if ( info.height() < 142 ) {
-		info.css( 'height', '10em' );
+	if ( info.height() < 158 ) {
+		info.css( 'height', '11em' );
 	}
 
 	var escapeHtml = function ( str ) {
