83f30cec8f1aceebe68cb79968340178dfe01e89
Allan Jardine <allan.jardine@sprymedia.co.uk>
2014-02-05 10:32:09 +0000

    Dev: Moving on to 1.10.0-beta.2.dev

--- media/js/jquery.dataTables.js
+++ media/js/jquery.dataTables.js
@@ -1,11 +1,11 @@
-/*! DataTables 1.10.0-beta.1
+/*! DataTables 1.10.0-beta.2.dev
  * ©2008-2014 SpryMedia Ltd - datatables.net/license
  */
 
 /**
  * @summary     DataTables
  * @description Paginate, search and order HTML tables
- * @version     1.10.0-beta.1
+ * @version     1.10.0-beta.2.dev
  * @file        jquery.dataTables.js
  * @author      SpryMedia Ltd (www.sprymedia.co.uk)
  * @contact     www.sprymedia.co.uk/contact
@@ -8652,7 +8652,7 @@
 	 *  @type string
 	 *  @default Version number
 	 */
-	DataTable.version = "1.10.0-beta.1";
+	DataTable.version = "1.10.0-beta.2.dev";
 
 	/**
 	 * Private data store, containing all of the settings objects that are
