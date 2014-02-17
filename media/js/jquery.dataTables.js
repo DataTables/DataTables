6968422b425bb4df7ad3cc9f0bdaa5bbca7877a9
Allan Jardine <allan.jardine@sprymedia.co.uk>
2014-02-17 10:18:54 +0000

    Fix: Ajax callback function wasn't being called when server-side processing was enabled (ajax.reload() and ajax.url().load())

--- media/js/jquery.dataTables.js
+++ media/js/jquery.dataTables.js
@@ -7108,10 +7108,17 @@
 				}
 	
 				_fnReDraw( settings, holdPosition );
+			} );
+		}
 	
-				if ( callback ) {
-					callback( json );
-				}
+		// Use the draw event to trigger a callback, regardless of if it is an async
+		// or sync draw
+		if ( callback ) {
+			var api = new _Api( settings );
+	
+			api.one( 'draw', function () {
+				console.log( api.ajax.json() );
+				callback( api.ajax.json() );
 			} );
 		}
 	};
