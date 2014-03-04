39828e8bab4aac2f8648df9aba486d6b1fb7a4c1
Allan Jardine <allan.jardine@sprymedia.co.uk>
2014-03-04 08:52:53 +0000

    Fix: Incorrect use of offsetHeight for scroll collapse

--- media/js/jquery.dataTables.js
+++ media/js/jquery.dataTables.js
@@ -3561,7 +3561,7 @@
 		// will end up forcing the scrollbar to appear, making our measurements wrong for when we
 		// then hide it (end of this function), so add the header height to the body scroller.
 		if ( scroll.bCollapse && scrollY !== "" ) {
-			divBodyStyle.height = (divBody.offsetHeight + header[0].offsetHeight)+"px";
+			divBodyStyle.height = (divBody[0].offsetHeight + header[0].offsetHeight)+"px";
 		}
 	
 		// Size the table as a whole
