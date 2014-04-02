85111b709c175d82ecfe5c4692d4ac15fd77819b
Allan Jardine <allan.jardine@sprymedia.co.uk>
2014-04-02 14:20:46 +0100

    Dev: Plain syntax highlighting

--- examples/resources/syntax/shCore.js
+++ examples/resources/syntax/shCore.js
@@ -1985,4 +1985,23 @@ typeof(exports) != 'undefined' ? exports.SyntaxHighlighter = SyntaxHighlighter :
 })();
 
 
+;(function()
+{
+    // CommonJS
+    SyntaxHighlighter = SyntaxHighlighter || (typeof require !== 'undefined'? require('shCore').SyntaxHighlighter : null);
+
+    function Brush()
+    {
+    };
+
+    Brush.prototype = new SyntaxHighlighter.Highlighter();
+    Brush.aliases   = ['text', 'plain'];
+
+    SyntaxHighlighter.brushes.Plain = Brush;
+
+    // CommonJS
+    typeof(exports) != 'undefined' ? exports.Brush = Brush : null;
+})();
+
+
 SyntaxHighlighter.all();
