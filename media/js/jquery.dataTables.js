bff1276ab6f6e794c23fbcd16df07a8e3e7179ca
Allan Jardine <allan.jardine@sprymedia.co.uk>
2014-04-11 09:09:24 +0100

    Dev: PHP output support for markdown, as it is used on the site in a few places

--- media/js/jquery.dataTables.js
+++ media/js/jquery.dataTables.js
@@ -1394,7 +1394,7 @@
 		// Are we reading last data from DOM or the data object?
 		if ( src === 'dom' || ((! src || src === 'auto') && row.src === 'dom') ) {
 			// Read the data from the DOM
-			row._aData = _fnGetRowElements( settings, row ).data;
+			row._aData = _fnGetRowElements( settings, row.nTr ).data;
 		}
 		else {
 			// Reading from data object, update the DOM
@@ -1432,8 +1432,7 @@
 	 * cells that are in the row.
 	 *
 	 * @param {object} settings DataTables settings object
-	 * @param {node|object} TR element from which to read data or existing row
-	 *   object from which to re-read the data from the cells
+	 * @param {node} TR element from which to read data
 	 * @returns {object} Object with two parameters: `data` the data read, in
 	 *   document order, and `cells` and array of nodes (they can be useful to the
 	 *   caller, so rather than needing a second traversal to get them, just return
@@ -1460,48 +1459,33 @@
 			}
 		};
 	
-		var cellProcess = function ( cell ) {
-			col = columns[i];
-			contents = $.trim(cell.innerHTML);
+		while ( td ) {
+			name = td.nodeName.toUpperCase();
 	
-			if ( col && col._bAttrSrc ) {
-				o = {
-					display: contents
-				};
+			if ( name == "TD" || name == "TH" ) {
+				col = columns[i];
+				contents = $.trim(td.innerHTML);
 	
-				attr( col.mData.sort, o, cell );
-				attr( col.mData.type, o, cell );
-				attr( col.mData.filter, o, cell );
+				if ( col && col._bAttrSrc ) {
+					o = {
+						display: contents
+					};
 	
-				d.push( o );
-			}
-			else {
-				d.push( contents );
-			}
+					attr( col.mData.sort, o, td );
+					attr( col.mData.type, o, td );
+					attr( col.mData.filter, o, td );
 	
-			tds.push( cell );
-			i++;
-		};
-	
-		if ( td ) {
-			// `tr` element passed in
-			while ( td ) {
-				name = td.nodeName.toUpperCase();
-	
-				if ( name == "TD" || name == "TH" ) {
-					cellProcess( td );
+					d.push( o );
+				}
+				else {
+					d.push( contents );
 				}
 	
-				td = td.nextSibling;
+				tds.push( td );
+				i++;
 			}
-		}
-		else {
-			// Existing row object passed in
-			tds = row.anCells;
-			
-			for ( var j=0, jen=tds.length ; j<jen ; j++ ) {
-				cellProcess( tds[j] );
-			}
+	
+			td = td.nextSibling;
 		}
 	
 		return {
