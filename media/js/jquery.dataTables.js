9a49208617626da147096dff79c84f92b0c9a908
Allan Jardine <allan.jardine@sprymedia.co.uk>
2014-10-08 11:28:33 +0100

    New: `dt-api $.fn.dataTable.util.throttle()` static utility method. This provides extension authors with an easy way to throttle function calls

--- media/js/jquery.dataTables.js
+++ media/js/jquery.dataTables.js
@@ -4170,7 +4170,7 @@
 	 */
 	function _fnThrottle( fn, freq ) {
 		var
-			frequency = freq || 200,
+			frequency = freq !== undefined ? freq : 200,
 			last,
 			timer;
 	
@@ -8733,6 +8733,29 @@
 				return o.nTable;
 			}
 		} );
+	};
+	
+	
+	/**
+	 * DataTables utility methods
+	 * 
+	 * This namespace provides helper methods that DataTables uses internally to
+	 * create a DataTable, but which are not exclusively used only for DataTables.
+	 * These methods can be used by extension authors to save the duplication of
+	 * code.
+	 *
+	 *  @namespace
+	 */
+	DataTable.util = {
+		/**
+		 * Throttle the calls to a function. Arguments and context are maintained
+		 * for the throttled function.
+		 *
+		 * @param {function} fn Function to be called
+		 * @param {integer} freq Call frequency in mS
+		 * @return {function} Wrapped function
+		 */
+		throttle: _fnThrottle
 	};
 	
 	
