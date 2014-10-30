e92582b45af59b4d95eca3165b76373dfc1172fe
Allan Jardine <allan.jardine@sprymedia.co.uk>
2014-10-30 15:15:20 +0000

    Fix: Page length change will now attempt to keep the start point on the current page, but will shift that row's position away from the start point, so that the paging is always consistent in its display. Without this changing the page length could result in landing on page "1.4" (or some other partial page) that you could never get to with just paging alone. That is no longer possible in DataTables.
    This is something that has been in DataTables since the version first
    vresion and was contentious at that point so while this is a minor code
    change, for me it is quite a big change! I feel that this is the correct
    operation now, although it does result in rows being moved around where
    they might not be needed to if partial pages could be used.

--- media/js/jquery.dataTables.js
+++ media/js/jquery.dataTables.js
@@ -5167,10 +5167,13 @@
 			len = settings._iDisplayLength;
 	
 		/* If we have space to show extra rows (backing up from the end point - then do so */
-		if (start >= end) 
+		if ( start >= end )
 		{
 			start = end - len;
 		}
+	
+		// Keep the start record on the current page
+		start -= (start % len);
 	
 		if ( len === -1 || start < 0 )
 		{
