0942fa38a661c510d6fe9f02da08628f8781b02e
Allan Jardine <allan.jardine@sprymedia.co.uk>
2015-03-03 15:42:22 +0000

    Fix: `dt-api cells()` when given a selector options object in the first parameter was incorrectly applying the selector for the cells.

--- media/js/jquery.dataTables.js
+++ media/js/jquery.dataTables.js
@@ -8481,13 +8481,15 @@
 		// Argument shifting
 		if ( $.isPlainObject( rowSelector ) ) {
 			// Indexes
-			if ( typeof rowSelector.row !== undefined ) {
-				opts = columnSelector;
-				columnSelector = null;
-			}
-			else {
+			if ( rowSelector.row === undefined ) {
+				// Selector options in first parameter
 				opts = rowSelector;
 				rowSelector = null;
+			}
+			else {
+				// Cell index objects in first parameter
+				opts = columnSelector;
+				columnSelector = null;
 			}
 		}
 		if ( $.isPlainObject( columnSelector ) ) {
