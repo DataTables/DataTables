fa9fa2e2968b19b87b38a1d1d2e80865e5a9e6d3
Allan Jardine <allan.jardine@sprymedia.co.uk>
2015-07-24 11:45:26 +0100

    Dev: Syntax highlighter - link urls with a comma in them Dev: Syntax highlighter - show the type label above the code so it doesn't over lap the code Dev: Syntax highlighter - don't show the `:after` option in highlighted code

--- examples/resources/syntax/shCore.js
+++ examples/resources/syntax/shCore.js
@@ -120,7 +120,7 @@ var sh = {
         multiLineDoubleQuotedString : XRegExp('"([^\\\\"]|\\\\.)*"', 'gs'),
         multiLineSingleQuotedString : XRegExp("'([^\\\\']|\\\\.)*'", 'gs'),
         xmlComments                 : XRegExp('(&lt;|<)!--.*?--(&gt;|>)', 'gs'),
-        url                         : /\w+:\/\/[\w-.\/?%&=:@;#]*/g,
+        url                         : /\w+:\/\/[\w-.\/?%&=:@;#,]*/g,
         phpScriptTags               : { left: /(&lt;|<)\?(?:=|php)?/g, right: /\?(&gt;|>)/g, 'eof' : true },
         aspScriptTags               : { left: /(&lt;|<)%=?/g, right: /%(&gt;|>)/g },
         scriptScriptTags            : { left: /(&lt;|<)\s*script.*?(&gt;|>)/gi, right: /(&lt;|<)\/\s*script\s*(&gt;|>)/gi }
