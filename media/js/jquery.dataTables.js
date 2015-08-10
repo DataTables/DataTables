9cc3be030f949034a66a19ed9ade390024d44ae2
Allan Jardine <allan.jardine@sprymedia.co.uk>
2015-08-10 17:47:34 +0100

    Fix: Calculation for horizontal scroll bar adjustment when the `dt-init scrollCollapse` parameter is set could be incorrect when the standard height exceeds the window height.
    - This is an interesting one, and I suspect a bit of an edge case in the
      browsers - what is happening is that the window is going from having a
      scrollbar to not having a scrollbar when the scrolling viewport is
      collapsed. This causes the browser to show the scrollbars in the
      viewport for an instant, even although they aren't actually required.
      If the height of the viewport is 53px and you up it to 80 so there are
      no scrollbars and reduce it back to 53 the scrollbars are gone! The
      browser must be stopping a race condition once the scrollbars are
      shown.
    
    - The fix is to use `max-height` for the scrolling container if the
      scroll collapse is set and `height` otherwise. The browser native
      methods makes the code _much_ easier to work with, more performant and
      easier to understand.

--- media/js/jquery.dataTables.js
+++ media/js/jquery.dataTables.js
@@ -3710,7 +3710,6 @@
 					.css( {
 						position: 'relative',
 						overflow: 'auto',
-						height: size( scrollY ),
 						width: size( scrollX )
 					} )
 					.append( table )
@@ -3756,6 +3755,11 @@
 				}
 			} );
 		}
+	
+		$(scrollBody).css(
+			scrollY && scroll.bCollapse ? 'max-height' : 'height', 
+			scrollY
+		);
 	
 		settings.nScrollHead = scrollHead;
 		settings.nScrollBody = scrollBody;
@@ -3871,13 +3875,6 @@
 			_fnApplyToChildren( function(n) {
 				n.style.width = "";
 			}, footerSrcEls );
-		}
-	
-		// If scroll collapse is enabled, when we put the headers back into the body for sizing, we
-		// will end up forcing the scrollbar to appear, making our measurements wrong for when we
-		// then hide it (end of this function), so add the header height to the body scroller.
-		if ( scroll.bCollapse && scrollY !== "" ) {
-			divBodyStyle.height = (divBody[0].offsetHeight + header[0].offsetHeight)+"px";
 		}
 	
 		// Size the table as a whole
@@ -4009,18 +4006,6 @@
 			 */
 			if ( ie67 ) {
 				divBodyStyle.height = _fnStringToCss( tableEl.offsetHeight+barWidth );
-			}
-		}
-	
-		if ( scrollY && scroll.bCollapse ) {
-			divBodyStyle.height = _fnStringToCss( scrollY );
-	
-			var iExtra = (scrollX && tableEl.offsetWidth > divBodyEl.offsetWidth) ?
-				barWidth :
-				0;
-	
-			if ( tableEl.offsetHeight < divBodyEl.offsetHeight ) {
-				divBodyStyle.height = _fnStringToCss( tableEl.offsetHeight+iExtra );
 			}
 		}
 	
