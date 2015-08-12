f91373337b56dc4e1f3be6f08f348ce9122e5598
Allan Jardine <allan.jardine@sprymedia.co.uk>
2015-08-12 19:49:47 +0100

    DataTables 1.10.8 release

--- media/js/jquery.dataTables.js
+++ media/js/jquery.dataTables.js
@@ -1,11 +1,11 @@
-/*! DataTables 1.10.8-dev
+/*! DataTables 1.10.8
  * ©2008-2014 SpryMedia Ltd - datatables.net/license
  */
 
 /**
  * @summary     DataTables
  * @description Paginate, search and order HTML tables
- * @version     1.10.8-dev
+ * @version     1.10.8
  * @file        jquery.dataTables.js
  * @author      SpryMedia Ltd (www.sprymedia.co.uk)
  * @contact     www.sprymedia.co.uk/contact
@@ -9224,7 +9224,7 @@
 	 *  @type string
 	 *  @default Version number
 	 */
-	DataTable.version = "1.10.8-dev";
+	DataTable.version = "1.10.8";
 
 	/**
 	 * Private data store, containing all of the settings objects that are
