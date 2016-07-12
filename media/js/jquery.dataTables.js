90173c16706d53caf4933ed338a11dc94257a182
JoniJnm <JoniJnm@users.noreply.github.com>
2016-07-12 17:18:15 +0200

    Improve throttle function

--- media/js/jquery.dataTables.js
+++ media/js/jquery.dataTables.js
@@ -1602,27 +1602,17 @@
 		throttle: function ( fn, freq ) {
 			var
 				frequency = freq !== undefined ? freq : 200,
-				last,
-				timer;
+				timer = 0;
 	
 			return function () {
 				var
 					that = this,
-					now  = +new Date(),
 					args = arguments;
-	
-				if ( last && now < last + frequency ) {
-					clearTimeout( timer );
-	
-					timer = setTimeout( function () {
-						last = undefined;
-						fn.apply( that, args );
-					}, frequency );
-				}
-				else {
-					last = now;
+					
+				clearTimeout( timer );
+				timer = setTimeout(function() {
 					fn.apply( that, args );
-				}
+				}, frequency);
 			};
 		},
 	
