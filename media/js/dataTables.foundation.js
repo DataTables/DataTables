71fcdec54b081d97c31b92cb067969499138789c
Allan Jardine <allan.jardine@sprymedia.co.uk>
2016-09-16 14:54:19 +0100

    Fix: Fooundation 6 styling for the processing element needs to use the `callout` class
    - Fixes DataTablesSrc #82

--- media/js/dataTables.foundation.js
+++ media/js/dataTables.foundation.js
@@ -47,7 +47,7 @@ meta.remove();
 
 $.extend( DataTable.ext.classes, {
 	sWrapper:    "dataTables_wrapper dt-foundation",
-	sProcessing: "dataTables_processing panel"
+	sProcessing: "dataTables_processing panel callout"
 } );
 
 
