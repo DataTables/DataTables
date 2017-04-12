3b24f99ac4ddb7f9072076b0d07f0b1a408f177a
Allan Jardine <allan.jardine@sprymedia.co.uk>
2017-04-12 20:00:44 +0100

    Fix - examples: Tighten up regex check to protect against LFI vulnerability
    Credit to Arnaud Labenne of Dotsafe.fr for finding and reporting this.

--- examples/resources/examples.php
+++ examples/resources/examples.php
@@ -1,6 +1,6 @@
 <?php
 
-if ( isset( $_POST['src'] ) && preg_match( '/scripts\/[a-zA-Z_\-_]+\.php/', $_POST['src'] ) !== 0 ) {
+if ( isset( $_POST['src'] ) && preg_match( '/^scripts\/[a-zA-Z_\-_]+\.php$/', $_POST['src'] ) !== 0 ) {
 	echo htmlspecialchars( file_get_contents( '../server_side/'.$_POST['src'] ) );
 }
 else {
