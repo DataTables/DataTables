d7886953966736dac4690ce1d8721ba8be71ec49
Allan Jardine <allan.jardine@sprymedia.co.uk>
2017-08-31 14:45:54 +0100

    Revert "Update: Change `-init dom` default to `lftrip` (move the processing element after the table). This is to allow correct layering when scrolling - previously the processing element wouldn't show if scrolling was enabled without a z-index value."
    This reverts commit ff352c4b077d1e0a558c03ff36e15d787560fafa.

--- media/js/jquery.dataTables.js
+++ media/js/jquery.dataTables.js
@@ -11808,7 +11808,8 @@
 		 *     </li>
 		 *   </ul>
 		 *  @type string
-		 *  @default lftrip
+		 *  @default lfrtip <i>(when `jQueryUI` is false)</i> <b>or</b>
+		 *    <"H"lfr>t<"F"ip> <i>(when `jQueryUI` is true)</i>
 		 *
 		 *  @dtopt Options
 		 *  @name DataTable.defaults.dom
@@ -11820,7 +11821,7 @@
 		 *      } );
 		 *    } );
 		 */
-		"sDom": "lftrip",
+		"sDom": "lfrtip",
 	
 	
 		/**
