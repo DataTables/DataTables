d894dc7fbc74e56d09507c0768e61c547f678074
Allan Jardine <allan.jardine@sprymedia.co.uk>
2017-08-31 14:49:38 +0100

    Fix - examples: Server-side processing example in data sources directory wasn't showing the server-side processing script.

--- examples/resources/examples.php
+++ examples/resources/examples.php
@@ -1,8 +1,12 @@
 <?php
 
-if ( isset( $_POST['src'] ) && preg_match( '/^scripts\/[a-zA-Z_\-_]+\.php$/', $_POST['src'] ) !== 0 ) {
+if ( isset( $_POST['src'] ) && (
+	preg_match( '/^scripts\/[a-zA-Z_\-_]+\.php$/', $_POST['src'] ) !== 0 ||
+	preg_match( '/^\.\.\/server_side\/scripts\/[a-zA-Z_\-_]+\.php$/', $_POST['src'] ) !== 0
+) ) {
 	echo htmlspecialchars( file_get_contents( '../server_side/'.$_POST['src'] ) );
 }
+
 else {
 	echo '';
 }
