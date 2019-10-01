620e9ca8bedb22067bcaa9fcfdba23343d4a5a78
Allan Jardine <allan@datatables.net>
2019-10-01 12:27:12 +0000

    Fix: Unable to use `-tag td` cells as a selector for `-api row()` and `-api rows()`
    - Thread https://datatables.net/forums/discussion/50511/

--- media/js/jquery.dataTables.js
+++ media/js/jquery.dataTables.js
@@ -1,11 +1,11 @@
-/*! DataTables 1.10.19
+/*! DataTables 1.10.19-dev
  * ©2008-2018 SpryMedia Ltd - datatables.net/license
  */
 
 /**
  * @summary     DataTables
  * @description Paginate, search and order HTML tables
- * @version     1.10.19
+ * @version     1.10.19-dev
  * @file        jquery.dataTables.js
  * @author      SpryMedia Ltd
  * @contact     www.datatables.net
@@ -7916,7 +7916,7 @@
 						[];
 				}
 				else if ( cellIdx ) {
-					return aoData[ cellIdx.row ] && aoData[ cellIdx.row ].nTr === sel ?
+					return aoData[ cellIdx.row ] && aoData[ cellIdx.row ].nTr === sel.parentNode ?
 						[ cellIdx.row ] :
 						[];
 				}
@@ -9463,7 +9463,7 @@
 	 *  @type string
 	 *  @default Version number
 	 */
-	DataTable.version = "1.10.19";
+	DataTable.version = "1.10.19-dev";
 
 	/**
 	 * Private data store, containing all of the settings objects that are
