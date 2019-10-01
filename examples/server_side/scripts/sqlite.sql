bda9b88ada89b56a7d73b03967ab9359b90320b1
Allan Jardine <allan@datatables.net>
2019-10-01 12:44:09 +0000

    Dev: Fix SQLite syntax - `timestamp without timezone` doesn't actually do anything other than `timestamp` in SQLite

--- examples/server_side/scripts/sqlite.sql
+++ examples/server_side/scripts/sqlite.sql
@@ -10,7 +10,7 @@ CREATE TABLE datatables_demo (
 	position   text NOT NULL default '',
 	email      text NOT NULL default '',
 	office     text NOT NULL default '',
-	start_date timestamp without time zone default NULL,
+	start_date timestamp default NULL,
 	age        integer,
 	salary     integer,
 	seq        integer,
