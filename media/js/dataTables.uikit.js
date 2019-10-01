2d6d87b222fe211b3ddfa7ddc8f86b4a0002b1c8
Allan Jardine <allan@datatables.net>
2019-10-01 13:02:44 +0000

    Fix: UIKit integration - better styling for filter and length inputs
    - https://github.com/DataTables/DataTablesSrc/commit/611c99c96ee44f38e566072feb5343994c999b90#commitcomment-32046169

--- media/js/dataTables.uikit.js
+++ media/js/dataTables.uikit.js
@@ -50,8 +50,8 @@ $.extend( true, DataTable.defaults, {
 /* Default class modification */
 $.extend( DataTable.ext.classes, {
 	sWrapper:      "dataTables_wrapper uk-form dt-uikit",
-	sFilterInput:  "uk-form-small",
-	sLengthSelect: "uk-form-small",
+	sFilterInput:  "uk-form-small uk-input",
+	sLengthSelect: "uk-form-small uk-select",
 	sProcessing:   "dataTables_processing uk-panel"
 } );
 
