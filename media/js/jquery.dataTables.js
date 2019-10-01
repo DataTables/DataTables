83657a29e33ce93ee940ce25684940eb3acb2913
Allan Jardine <allan@datatables.net>
2019-10-01 13:42:32 +0000

    Release 1.10.20

--- media/js/jquery.dataTables.js
+++ media/js/jquery.dataTables.js
@@ -1,15 +1,15 @@
-/*! DataTables 1.10.20-dev
- * ©2008-2018 SpryMedia Ltd - datatables.net/license
+/*! DataTables 1.10.20
+ * ©2008-2019 SpryMedia Ltd - datatables.net/license
  */
 
 /**
  * @summary     DataTables
  * @description Paginate, search and order HTML tables
- * @version     1.10.20-dev
+ * @version     1.10.20
  * @file        jquery.dataTables.js
  * @author      SpryMedia Ltd
  * @contact     www.datatables.net
- * @copyright   Copyright 2008-2018 SpryMedia Ltd.
+ * @copyright   Copyright 2008-2019 SpryMedia Ltd.
  *
  * This source file is free software, available under the following license:
  *   MIT license - http://datatables.net/license
@@ -9487,7 +9487,7 @@
 	 *  @type string
 	 *  @default Version number
 	 */
-	DataTable.version = "1.10.20-dev";
+	DataTable.version = "1.10.20";
 
 	/**
 	 * Private data store, containing all of the settings objects that are
